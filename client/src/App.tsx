a46d96c66541a1db5eab27430f2881f84d2c8e2c
noritaket28 <38028287-noritaket28@users.noreply.replit.com>
2024-12-06 06:06:56 +0000

    Agent query: Can you see the 'Go to Auth Page' link on the home page, and when clicked, does it take you to a page that displays just the text 'auth'?

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import LoginPage from "@/pages/LoginPage";
+import AuthPage from "@/pages/AuthPage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </Router>
   );
